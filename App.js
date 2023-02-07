4f5144328069f0a53bc43e1b052e6ecee5a8c78a
Sanjana Gudla <sanjana.gudla@ibigroup.com>
2023-02-07 23:42:00 +0530

    chapter 2

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 // const heading =  document.createElement("h1");
 // heading.innerHTML = "This is from JS";
 // const root = document.getElementById('root');
